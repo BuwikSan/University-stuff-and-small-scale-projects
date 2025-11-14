507f92d593ca4540f993a7e905a99102fff5b174
Burymuru <matbx6@gmail.com>
2025-11-14 10:23:25 +0100

    a

--- 1_University/3_semestr/oonv_project/Program.cs
+++ 1_University/3_semestr/oonv_project/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace oonv;
+﻿using oonv.bridge;
+
+namespace oonv;
 class Program
 {
     static void Main(string[] args)
     {
         ZatutejATimPoslesBitcoin app = new ZatutejATimPoslesBitcoin();
         app.TutProPenize();
+        TestBridge bridgeTest = new TestBridge();
+        bridgeTest.TestMe();
     }
 }
