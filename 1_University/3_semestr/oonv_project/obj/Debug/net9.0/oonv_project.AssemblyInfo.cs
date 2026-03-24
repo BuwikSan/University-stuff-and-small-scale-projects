5462dbd457f257f8c08dad6ac5fb09731e02adf0
Burymuru <matbx6@gmail.com>
2026-03-24 12:44:12 +0100

    a

--- 1_University/3_semestr/oonv_project/obj/Debug/net9.0/oonv_project.AssemblyInfo.cs
+++ 1_University/3_semestr/oonv_project/obj/Debug/net9.0/oonv_project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("oonv_project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7209669c1be9238fed3ea88572a87af3362073a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f844ec0397b9ce15d701264c9f05a35b227e4c58")]
 [assembly: System.Reflection.AssemblyProductAttribute("oonv_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("oonv_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
